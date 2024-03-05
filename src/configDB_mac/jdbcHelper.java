f0657db95a262524ee655721fe76dce5deddd1da
ngochungsoftware <hungit2301@gmail.com>
2024-03-05 18:46:10 +0700

    feat: base

---
+++ src/configDB_mac/jdbcHelper.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package configDB_mac;
+
+/**
+ *
+ * @author macbook
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class jdbcHelper {
+
+    private static final String USERNAME = "SA";
+    private static final String PASSWORD = "example_123";
+    private static final String SERVER = "localhost";
+    private static final String PORT = "1433";
+    private static final String DATABASE_NAME = "Edu_Book_Store";
+    private static final boolean USING_SSL = true;
+
+    private static String CONNECT_STRING;
+
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            StringBuilder connectStringBuilder = new StringBuilder();
+            connectStringBuilder.append("jdbc:sqlserver://")
+                    .append(SERVER).append(":").append(PORT).append(";")
+                    .append("databaseName=").append(DATABASE_NAME).append(";")
+                    .append("user=").append(USERNAME).append(";")
+                    .append("password=").append(PASSWORD).append(";");
+            if (USING_SSL) {
+                connectStringBuilder.append("encrypt=true;trustServerCertificate=true;");
+            }
+            CONNECT_STRING = connectStringBuilder.toString();
+            System.out.println("Connect String có dạng: " + CONNECT_STRING);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(jdbcHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(CONNECT_STRING);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(jdbcHelper.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public static PreparedStatement getStmt(String sql, Object... args) throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement stmt;
+        if (sql.trim().startsWith("[")) {
+            stmt = conn.prepareCall(sql);//PROC
+        } else {
+            stmt = conn.prepareStatement(sql);//SQL
+        }
+        for (int i = 0; i < args.length; i++) {
+
+            stmt.setObject(i + 1, args[i]);
+        }
+        return stmt;
+    }
+
+    public static ResultSet query(String sql, Object... args) throws SQLException {
+        PreparedStatement stmt = jdbcHelper.getStmt(sql, args);
+        return stmt.executeQuery();
+    }
+
+    public static int update(String sql, Object... args) {
+        try {
+            PreparedStatement stmt = jdbcHelper.getStmt(sql, args);
+            try {
+                return stmt.executeUpdate();
+            } finally {
+                stmt.getConnection().close();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static Object value(String sql, Object... args) {
+        try {
+            ResultSet rs = jdbcHelper.query(sql, args);
+            if (rs.next()) {
+                return rs.getObject(0);
+            }
+            rs.getStatement().getConnection().close();
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+}
