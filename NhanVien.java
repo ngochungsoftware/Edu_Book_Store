53afb662563c84ba7d7a4a5434c4fcacc4a7b757
6thFebruary <150697134+6thFebruary@users.noreply.github.com>
2024-03-13 17:04:29 +0700

    Create NhanVien.java

---
+++ NhanVien.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class NhanVien {
+    int MaNV;
+    String HoTen;
+    String TaiKhoan;
+    String MatKhau;
+    String Email;
+    String DiaChi;
+    String DienThoai;
+    String GioiTinh;
+    String NgaySinh;
+    String Vaitro;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(int MaNV, String HoTen, String TaiKhoan, String MatKhau, String Email, String DiaChi, String DienThoai, String GioiTinh, String NgaySinh, String Vaitro) {
+        this.MaNV = MaNV;
+        this.HoTen = HoTen;
+        this.TaiKhoan = TaiKhoan;
+        this.MatKhau = MatKhau;
+        this.Email = Email;
+        this.DiaChi = DiaChi;
+        this.DienThoai = DienThoai;
+        this.GioiTinh = GioiTinh;
+        this.NgaySinh = NgaySinh;
+        this.Vaitro = Vaitro;
+    }
+
+    public int getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(int MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public String getTaiKhoan() {
+        return TaiKhoan;
+    }
+
+    public void setTaiKhoan(String TaiKhoan) {
+        this.TaiKhoan = TaiKhoan;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String MatKhau) {
+        this.MatKhau = MatKhau;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public String getDienThoai() {
+        return DienThoai;
+    }
+
+    public void setDienThoai(String DienThoai) {
+        this.DienThoai = DienThoai;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public String getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(String NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public String getVaitro() {
+        return Vaitro;
+    }
+
+    public void setVaitro(String Vaitro) {
+        this.Vaitro = Vaitro;
+    }
+
+    @Override
+    public String toString() {
+        return "NhanVien{" + "MaNV=" + MaNV + ", HoTen=" + HoTen + ", TaiKhoan=" + TaiKhoan + ", MatKhau=" + MatKhau + ", Email=" + Email + ", DiaChi=" + DiaChi + ", DienThoai=" + DienThoai + ", GioiTinh=" + GioiTinh + ", NgaySinh=" + NgaySinh + ", Vaitro=" + Vaitro + '}';
+    }
+    
+    
+}
